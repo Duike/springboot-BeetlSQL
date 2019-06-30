926c00a4aa6da2aaace3b214e35c56857276aa7c
duike <405971652@qq.com>
2019-06-30 15:38:20 +0800

    初始化文件

---
+++ src/main/java/com/zile/beetlsql/BeetlSqlApplication.java
@@ -0,0 +1,13 @@
+package com.zile.beetlsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetlSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeetlSqlApplication.class, args);
+    }
+
+}
